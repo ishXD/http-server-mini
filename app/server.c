5473ae7b8b9c3bdd6b365c2f7fb414400116db2b
isha <ishajayal1@gmail.com>
2024-06-06 13:03:01 +0530

    test 2

--- app/server.c
+++ app/server.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
+#include <winsock.h>
+//#include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <string.h>

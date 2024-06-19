e933e2710c704c499d023ea8b07a02249873e4a4
isha <ishajayal1@gmail.com>
2024-06-19 22:06:47 +0530

    Create file endpoint

--- app/server.c
+++ app/server.c
@@ -40,20 +40,18 @@ void *handle_request(void *socket_desc){
 
 			char *file_requested = url + 7;
 
-			char file_path[BUFFER_SIZE];
+			char *file_path = strcat(directory,file_requested);
 
-			snprintf(file_path, sizeof(file_path), "%s%s", directory, file_requested);
-			printf("%s", file_path);
-
-
+			//snprintf(file_path, sizeof(file_path), "%s%s", directory, file_requested);
+			
 			FILE *file = fopen(file_path,"r");
 			if(file == NULL)
 				snprintf(response, sizeof(response),"HTTP/1.1  404 Not Found\r\n\r\n\r\n");
 
 			else{
-				char file_buffer[BUFFER_SIZE];
-				int read = fread(file_buffer, 1, sizeof(file_buffer) - 1, file);
-				file_buffer[read] = '\0';
+				char *file_buffer[BUFFER_SIZE];
+				int bytes_read = fread(file_buffer, 1, sizeof(file_buffer) - 1, file);
+				file_buffer[bytes_read] = '\0';
 				fclose(file);
 
 				snprintf(response, sizeof(response), "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", strlen(file_buffer), file_buffer);
